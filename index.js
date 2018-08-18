dacaa92954590481f35342ac8f8cf0f5dd6d494f
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2018-08-18 13:22:34 +0200

    pre final

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Service, Characteristic, HomebridgeAPI, FakeGatoHistoryService;
 var inherits = require('util').inherits;
 var os = require("os");
 var hostname = os.hostname();
+var request = require("request");
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
@@ -11,6 +12,36 @@ module.exports = function (homebridge) {
 
     homebridge.registerAccessory("homebridge-aquarium", "Aquarium", AquariumPlugin);
 };
+
+
+var ph;
+var temp;
+var niveau;
+var url;
+var body;
+
+
+function read() {
+	
+    request(url, function (error, response, body) {
+		var pos = body.indexOf("pH-Wert 1");
+		var str = body.substr(pos + 145, 6);
+		for (i = 0; i < 6; i++)
+			if (str[i] != ' ') break;
+		if (i) str = str.substr(i);
+		ph = parseFloat(str);
+		
+		pos = body.indexOf("Temperatur 1");
+		str = body.substr(pos + 149, 8);
+		for (i = 0; i < 8; i++)
+			if (str[i] != ' ') break;
+		if (i) str = str.substr(i);
+		temp = parseFloat(str);
+
+		pos = body.indexOf("Niveauregelung 1");
+		niveau = body.substr(pos + 118, 3);
+	});
+} 
 
 
 function AquariumPlugin(log, config) {
@@ -23,79 +54,59 @@ function AquariumPlugin(log, config) {
 
     this.config = config;
 
-    this.storedData = {};
-
-    if (config.humidityAlertLevel != null) {
-        this.humidityAlert = true;
-        this.humidityAlertLevel = config.humidityAlertLevel;
-    } else {
-        this.humidityAlert = false;
-    }
-
-    if (config.lowLightAlertLevel != null) {
-        this.lowLightAlert = true;
-        this.lowLightAlertLevel = config.lowLightAlertLevel;
-    } else {
-        this.lowLightAlert = false;
-    }
+	url = "http://192.168.178.21/sensors.html";
 
     // Setup services
     this.setUpServices();
+        
+	read();
+
+	setInterval(function() {
+					
+		if (true) {
+			
+			read();
+			
+			that.log("pH Wert: " + ph + " Temperatur: " + temp + " Leakage: " + niveau);
+
+			that.fakeGatoHistoryService.addEntry({
+				time: new Date().getTime() / 1000,
+				temp: temp,
+				pressure: ph * 1000
+				});
+		}
+	}, 60000); // ev 1 min
+	
 }
 
 
 AquariumPlugin.prototype.getFirmwareRevision = function (callback) {
-    callback(null, this.storedData.firmware ? this.storedData.firmware.firmwareVersion : '0.0.0');
+    callback(null, '0.0.0');
 };
 
 AquariumPlugin.prototype.getBatteryLevel = function (callback) {
-    callback(null, this.storedData.firmware ? this.storedData.firmware.batteryLevel : 0);
+    callback(null, 100);
 };
 
 AquariumPlugin.prototype.getStatusActive = function (callback) {
-    callback(null, this.storedData.data ? true : false);
+    callback(null, true);
 };
 
 AquariumPlugin.prototype.getStatusLowBattery = function (callback) {
-    if (this.storedData.firmware) {
-        callback(null, this.storedData.firmware.batteryLevel <= 20 ? Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
-    } else {
-        callback(null, Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
-    }
+    callback(null, Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
 };
 
-AquariumPlugin.prototype.getStatusLowMoisture = function (callback) {
-    if (this.storedData.data) {
-        callback(null, this.storedData.data.moisture <= this.humidityAlertLevel ? Characteristic.ContactSensorState.CONTACT_NOT_DETECTED : Characteristic.ContactSensorState.CONTACT_DETECTED);
-    } else {
-        callback(null, Characteristic.ContactSensorState.CONTACT_DETECTED);
-    }
-};
-
-AquariumPlugin.prototype.getStatusLowLight = function (callback) {
-    if (this.storedData.data) {
-        callback(null, this.storedData.data.lux <= this.lowLightAlertLevel ? Characteristic.ContactSensorState.CONTACT_NOT_DETECTED : Characteristic.ContactSensorState.CONTACT_DETECTED);
-    } else {
-        callback(null, Characteristic.ContactSensorState.CONTACT_DETECTED);
-    }
-};
-
-AquariumPlugin.prototype.getCurrentAmbientLightLevel = function (callback) {
-    callback(null, this.storedData.data ? this.storedData.data.lux : 0);
-};
+AquariumPlugin.prototype.getStatusLeak = function (callback) {
+    callback(null, true ? Characteristic.ContactSensorState.CONTACT_NOT_DETECTED : Characteristic.ContactSensorState.CONTACT_DETECTED);
+ };
 
 AquariumPlugin.prototype.getCurrentTemperature = function (callback) {
-    callback(null, this.storedData.data ? this.storedData.data.temperature : 0);
+    callback(null, temp);
 };
 
 AquariumPlugin.prototype.getCurrentMoisture = function (callback) {
-    callback(null, this.storedData.data ? this.storedData.data.moisture : 0);
+    callback(null, ph * 1000);
 };
-
-AquariumPlugin.prototype.getCurrentFertility = function (callback) {
-    callback(null, this.storedData.data ? this.storedData.data.fertility : 0);
-};
-
 
 AquariumPlugin.prototype.setUpServices = function () {
     // info service
@@ -114,15 +125,7 @@ AquariumPlugin.prototype.setUpServices = function () {
     this.batteryService.getCharacteristic(Characteristic.StatusLowBattery)
         .on('get', this.getStatusLowBattery.bind(this));
 
-    this.lightService = new Service.LightSensor(this.name);
-    this.lightService.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
-        .on('get', this.getCurrentAmbientLightLevel.bind(this));
-    this.lightService.getCharacteristic(Characteristic.StatusLowBattery)
-        .on('get', this.getStatusLowBattery.bind(this));
-    this.lightService.getCharacteristic(Characteristic.StatusActive)
-        .on('get', this.getStatusActive.bind(this));
-
-    this.tempService = new Service.TemperatureSensor(this.name);
+    this.tempService = new Service.TemperatureSensor("Temperatur");
     this.tempService.getCharacteristic(Characteristic.CurrentTemperature)
         .on('get', this.getCurrentTemperature.bind(this));
     this.tempService.getCharacteristic(Characteristic.StatusLowBattery)
@@ -130,31 +133,13 @@ AquariumPlugin.prototype.setUpServices = function () {
     this.tempService.getCharacteristic(Characteristic.StatusActive)
         .on('get', this.getStatusActive.bind(this));
 
-    this.humidityService = new Service.HumiditySensor(this.name);
-    this.humidityService.getCharacteristic(Characteristic.CurrentRelativeHumidity)
-        .on('get', this.getCurrentMoisture.bind(this));
-    this.humidityService.getCharacteristic(Characteristic.StatusLowBattery)
-        .on('get', this.getStatusLowBattery.bind(this));
-    this.humidityService.getCharacteristic(Characteristic.StatusActive)
-        .on('get', this.getStatusActive.bind(this));
-
-    if (this.humidityAlert) {
-        this.humidityAlertService = new Service.ContactSensor(this.name + " Low Humidity", "humidity");
+    if (true) {
+        this.humidityAlertService = new Service.ContactSensor(this.name + " leak detected", "leak");
         this.humidityAlertService.getCharacteristic(Characteristic.ContactSensorState)
-            .on('get', this.getStatusLowMoisture.bind(this));
+            .on('get', this.getStatusLeak.bind(this));
         this.humidityAlertService.getCharacteristic(Characteristic.StatusLowBattery)
             .on('get', this.getStatusLowBattery.bind(this));
         this.humidityAlertService.getCharacteristic(Characteristic.StatusActive)
-            .on('get', this.getStatusActive.bind(this));
-    }
-
-    if (this.lowLightAlert) {
-        this.lowLightAlertService = new Service.ContactSensor(this.name + " Low Light", "light");
-        this.lowLightAlertService.getCharacteristic(Characteristic.ContactSensorState)
-            .on('get', this.getStatusLowLight.bind(this));
-        this.lowLightAlertService.getCharacteristic(Characteristic.StatusLowBattery)
-            .on('get', this.getStatusLowBattery.bind(this));
-        this.lowLightAlertService.getCharacteristic(Characteristic.StatusActive)
             .on('get', this.getStatusActive.bind(this));
     }
 
@@ -164,13 +149,13 @@ AquariumPlugin.prototype.setUpServices = function () {
         own characteristics and services
     */
 
-    // moisture characteristic
+    // pH characteristic
     SoilMoisture = function () {
-        Characteristic.call(this, 'Soil Moisture', 'C160D589-9510-4432-BAA6-5D9D77957138');
+        Characteristic.call(this, 'Soil Moisture', 'E863F10F-079E-48FF-8F27-9C2605A29F52');
         this.setProps({
-            format: Characteristic.Formats.UINT8,
-            unit: Characteristic.Units.PERCENTAGE,
-            maxValue: 100,
+            format: Characteristic.Formats.UINT16,
+            unit: "pH",
+            maxValue: 9000,
             minValue: 0,
             minStep: 0.1,
             perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
@@ -180,29 +165,12 @@ AquariumPlugin.prototype.setUpServices = function () {
 
     inherits(SoilMoisture, Characteristic);
 
-    SoilMoisture.UUID = 'C160D589-9510-4432-BAA6-5D9D77957138';
+    SoilMoisture.UUID = 'E863F10F-079E-48FF-8F27-9C2605A29F52';
 
 
-    // fertility characteristic
-    SoilFertility = function () {
-        Characteristic.call(this, 'Soil Fertility', '0029260E-B09C-4FD7-9E60-2C60F1250618');
-        this.setProps({
-            format: Characteristic.Formats.UINT8,
-            maxValue: 10000,
-            minValue: 0,
-            minStep: 1,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
 
-    inherits(SoilFertility, Characteristic);
-
-    SoilFertility.UUID = '0029260E-B09C-4FD7-9E60-2C60F1250618';
-
-
-    // moisture sensor
-    PlantSensor = function (displayName, subtype) {
+    // aquarium sensor
+    AquaSensor = function (displayName, subtype) {
         Service.call(this, displayName, '3C233958-B5C4-4218-A0CD-60B8B971AA0A', subtype);
 
         // Required Characteristics
@@ -210,29 +178,27 @@ AquariumPlugin.prototype.setUpServices = function () {
 
         // Optional Characteristics
         this.addOptionalCharacteristic(Characteristic.CurrentTemperature);
-        this.addOptionalCharacteristic(SoilFertility);
     };
 
-    inherits(PlantSensor, Service);
+    inherits(AquaSensor, Service);
 
-    PlantSensor.UUID = '3C233958-B5C4-4218-A0CD-60B8B971AA0A';
+    AquaSensor.UUID = '3C233958-B5C4-4218-A0CD-60B8B971AA0A';
 
-
-    this.plantSensorService = new PlantSensor(this.name);
-    this.plantSensorService.getCharacteristic(SoilMoisture)
+    this.aquaSensorService = new AquaSensor(this.name);
+    this.aquaSensorService.getCharacteristic(SoilMoisture)
         .on('get', this.getCurrentMoisture.bind(this));
-    this.plantSensorService.getCharacteristic(SoilFertility)
-        .on('get', this.getCurrentFertility.bind(this));
+    this.aquaSensorService.getCharacteristic(Characteristic.CurrentTemperature)
+        .on('get', this.getCurrentTemperature.bind(this));
+    this.aquaSensorService.getCharacteristic(Characteristic.ContactSensorState)
+        .on('get', this.getStatusLeak.bind(this));
 };
 
 
 AquariumPlugin.prototype.getServices = function () {
-    var services = [this.informationService, this.batteryService, this.lightService, this.tempService, this.humidityService, this.plantSensorService, this.fakeGatoHistoryService];
-    if (this.humidityAlert) {
-        services[services.length] = this.humidityAlertService;
-    }
-    if (this.lowLightAlert) {
-        services[services.length] = this.lowLightAlertService;
-    }
+    var services = [this.informationService, 
+					this.batteryService, 
+					this.aquaSensorService, 
+					this.fakeGatoHistoryService];
+
     return services;
 };
